36b2180a2c0ebf0870f719bc71ea4a2128c8b392
Mouez98 <mouez.matoui@gmail.com>
2022-01-28 23:32:58 +0100

    Final

--- src/App.js
+++ src/App.js
@@ -6,19 +6,16 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [colorsList, setColorsList] = useState([]);
+  const [colorsList, setColorsList] = useState(new Values("#0ff").all(10));
   const onSubmitHandler = (e) => {
     e.preventDefault();
 
    try { 
      const colors = new Values(color).all(10);
      setColorsList(colors)
-    //  console.log(colors)
-    //  colors.map(color => console.log(color.rgb, color.type))
-    setError(false)
+     setError(false)
   } catch(error) {
-    setError(true)
-    console.log(error)
+      setError(true)
   }
   };
 
@@ -29,7 +26,7 @@ function App() {
         <form onSubmit={onSubmitHandler}>
           <input
             type="text"
-            value={color}
+            defaultValue={'#0ff'}
             onChange={(e) => setColor(e.target.value)}
             className={`${error ? 'error' : ''}`}
             
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -4,12 +4,25 @@ import React, { useState, useEffect } from "react";
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(",");
-  // const hex = rgbToHex(...rgb)
+  const hex = `#${hexColor}`;
+  
+  const copieColorHandler = () => {
+    setAlert(true)
+    navigator.clipboard.writeText(hex)
+    setTimeout(()=>{
+        setAlert(false)
+    }, 2000)
+  }
 
   return (
-    <div className={`color ${index > 10 && "color-light"}`} style={{ backgroundColor: `rgb(${bcg})` }}>
+    <div
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={copieColorHandler}
+    >
       <p className="percent-value">{weight}%</p>
-      <p className="color-value">#{hexColor}</p>
+      <p className="color-value">{hex}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
     </div>
   );
 };
